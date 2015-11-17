783ccc97720278e057ea64759c9c2dfdcf853e3e
efarrell <elisakfarrell@gmail.com>
2015-11-16 21:47:14 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+$("#fblogin").click(function(e){
+  e.preventDefault();
+  console.info("logging in through facebook");
+
+  Parse.FacebookUtils.logIn(null, {
+    success: function(user) {
+      if (!user.existed()) {
+        console.info("User signed up and logged in through Facebook!");
+      } else {
+        console.info("User logged in through Facebook!");
+      }
+    },
+    error: function(user, error) {
+      console.info("User cancelled the Facebook login or did not fully authorize.");
+    }
+  });
+})
---
+++ public/js/fb.js
@@ -0,0 +1,20 @@
+window.fbAsyncInit = function() {
+  Parse.FacebookUtils.init({
+    appId      : '189377341402438',
+    status     : true,
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v2.5'
+  });
+
+  // Run code after the Facebook SDK is loaded.
+  console.info("facebook sdk loaded");
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
